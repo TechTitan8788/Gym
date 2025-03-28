851efd1653767772a0968d7807ce102f3e25e841
Tech Titans <lokhandeVishal8788@gmail.com>
2025-03-28 18:26:09 +0000

    ui done

---
+++ src/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+import Home from './components/Home/Home';
+
+function Layout() {
+    return (
+        <>
+        <Header/>
+        <Home/>
+        <Outlet/>
+        <Footer/>
+
+        </>
+        );
+}
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Layout from './Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
